2553ea36070843d44975a81a52c1ab459217f0e6
SCIPIO666 <eaphoney@gmail.com>
2026-05-28 14:35:47 +0300

    fix: debugging init

--- server/tests/trainers/trainers.test.js
+++ server/tests/trainers/trainers.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../../server');
+const app = require('../../app');
 const { cleanDatabase, createTestUser, prisma } = require('../helpers/dbHelper');
 const { setupTestTokens } = require('../helpers/authHelper');
 
