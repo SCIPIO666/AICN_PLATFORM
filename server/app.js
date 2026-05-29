360fbefc3eeb612febcc3b92b710ecc8f2bc5967
SCIPIO666 <eaphoney@gmail.com>
2026-05-29 13:36:51 +0300

    feat: updated swagger docs,api endpoits aligned

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const setupSwagger = require('./config/swagger');
 const devLogger = require('./utils/logger');
+const errorHandler = require('./middleware/errorHandler');
+const { notFoundHandler } = require('./middleware/notFound');
+
 
 //routers
 const authRouter = require('./routes/authRoutes');
@@ -44,69 +47,11 @@ app.get('/api/v1/health', (req, res) => {
   res.status(200).json({ status: 'OK', timestamp: new Date() });
 });
 
-//pdf generator test
-app.get('/api/v1/pdf/debug', async (req, res) => {
+// 404 handler 
+app.use(notFoundHandler);
 
-  try {
-
-    const example = require('./utils/pdf/templates/example');
-    const {generatePdf} = require('./utils/pdf/service/pdfGenerator');
-    const data = {
-      name: 'John Doe',
-      age: 34,
-      reportId: 'RPT-2024-001',
-      results: [
-        { test: 'Glucose', result: 'Normal', normalRange: '70-99 mg/dL' },
-        { test: 'HB', result: '13.5', normalRange: '13.5-17.5 g/dL' },
-        { test: 'Cholesterol', result: '190', normalRange: '<200 mg/dL' }
-      ]
-    };
-
-    const html = example(data);
-    const pdfBuffer = await generatePdf(html);
-
-    res.writeHead(200, {
-      'Content-Type': 'application/pdf',
-
-      'Content-Length': pdfBuffer.length,
-
-      'Content-Disposition':
-        'inline; filename="debug.pdf"',
-    });
-    res.end(pdfBuffer);
-
-  } catch (error) {
-
-    console.error(
-      'PDF generation failed:',
-      error
-    );
-
-    res.status(500).send(error.message);
-  }
-});
-//  404 error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  //  error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Global error handler 
+app.use(errorHandler);
 
 
-app.listen(process.env.PORT,'localhost',()=>{
-  devLogger.info(`\n Server is running! on port ${process.env.PORT}`);
-  devLogger.info(`API URL: http://localhost:${process.env.PORT}/api`);
-  devLogger.info(` Swagger UI: http://localhost:${process.env.PORT}/api-docs`);
-  devLogger.info(` Swagger UI: http://localhost:${process.env.PORT}/swagger.json`);
-  devLogger.info(` Health check: http://localhost:${process.env.PORT}/api/v1/health\n`);
-  
-})
 module.exports = app;
