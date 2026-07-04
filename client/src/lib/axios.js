62cf471d594bf822d07caf8d15105166454e32b5
SCIPIO666 <eaphoney@gmail.com>
2026-07-04 16:01:55 +0300

    fix: axios instance

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,14 +2,13 @@ import axios from 'axios'
 
 // instance
 const api = axios.create({
-
-    // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
-        baseURL: '/api/v1',//debug
-
+    baseURL: import.meta.env.VITE_API_URL
+        ? `${import.meta.env.VITE_API_URL}/api/v1`
+        : '/api/v1',
     headers: {
         "Content-Type": "application/json"
     },
-     withCredentials: true 
+    withCredentials: true
 })
 
 // interceptors - request
